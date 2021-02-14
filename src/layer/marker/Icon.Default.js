61fbbca09a1ddf7bed5df8999992dbbf92d3ae62
Nick Labsvirs <nicklabsvirs@gmail.com>
2021-02-14 10:39:14 +0100

    Add console log and add NLTEST1

--- src/layer/marker/Icon.Default.js
+++ src/layer/marker/Icon.Default.js
@@ -27,7 +27,7 @@ export var IconDefault = Icon.extend({
 		iconAnchor:  [12, 41],
 		popupAnchor: [1, -34],
 		tooltipAnchor: [16, -28],
-		shadowSize:  [41, 41]
+		shadowSize:  [410, 410]
 	},
 
 	_getIconUrl: function (name) {
